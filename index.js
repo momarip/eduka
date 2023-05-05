0745e791fda4d2d985b43150b7233cc7e796d650
Mohammed EL YOUSFI <momarip.el.yousfi@gmail.com>
2023-05-05 14:51:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,27 +4,27 @@ const chrome = require('selenium-webdriver/chrome');
 const PORT = 3000;
 
 const server = http.createServer(async (req, res) => {
-  let driver = await new Builder().forBrowser('chrome').setChromeOptions(new chrome.Options().headless()).build();
+  // let driver = await new Builder().forBrowser('chrome').setChromeOptions(new chrome.Options().headless()).build();
   
-  try {
+  //try {
     //  Navigate to Google.com
-    await driver.get('https://www.google.com/');
+    //await driver.get('https://www.google.com/');
 
     // Fnd the search input field and enter a search query
-    let searchInput = await driver.findElement(By.name('q'));
-    await searchInput.sendKeys('Selenium WebDriver', Key.RETURN);
+    //let searchInput = await driver.findElement(By.name('q'));
+    //await searchInput.sendKeys('Selenium WebDriver', Key.RETURN);
 
     // Wait for the search results to load and get the page title
-    await driver.wait(until.titleContains('Selenium WebDriver'), 5000);
-    let pageTitle = await driver.getTitle();
+    //await driver.wait(until.titleContains('Selenium WebDriver'), 5000);
+    //let pageTitle = await driver.getTitle();
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    console.log(pageTitle);
-    res.end(pageTitle);
-  } finally {
+    //console.log();
+    res.end("");
+  //} finally {
         // Quit the browser
-        await driver.quit();
-  }
+    //    await driver.quit();
+  //}
   
 });
 
