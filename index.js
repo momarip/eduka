28b888172eb04b9fc2097528dbab02c1b1ced3b6
Mohammed EL YOUSFI <momarip.el.yousfi@gmail.com>
2023-05-05 14:18:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const http = require('http');
+const { Builder, By, Key, until } = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
