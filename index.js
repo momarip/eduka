bd0aeeafadf89db49fc044e52acc8bba26e9778e
Mohammed EL YOUSFI <momarip.el.yousfi@gmail.com>
2023-05-05 14:01:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,27 +10,27 @@ const server = http.createServer(async (req, res) => {
     .forBrowser('chrome')
     .setChromeOptions(new chrome.Options().headless())
     .build();
+    console.log(driver);
+//   try {
+//     // Navigate to Google.com
+//     await driver.get('https://www.google.com/');
 
-  try {
-    // Navigate to Google.com
-    await driver.get('https://www.google.com/');
+//     // Find the search input field and enter a search query
+//     let searchInput = await driver.findElement(By.name('q'));
+//     await searchInput.sendKeys('Selenium WebDriver', Key.RETURN);
 
-    // Find the search input field and enter a search query
-    let searchInput = await driver.findElement(By.name('q'));
-    await searchInput.sendKeys('Selenium WebDriver', Key.RETURN);
+//     // Wait for the search results to load and get the page title
+//     await driver.wait(until.titleContains('Selenium WebDriver'), 5000);
+//     let pageTitle = await driver.getTitle();
 
-    // Wait for the search results to load and get the page title
-    await driver.wait(until.titleContains('Selenium WebDriver'), 5000);
-    let pageTitle = await driver.getTitle();
-
-    // Send the page title as the response
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end(pageTitle);
-  } finally {
-    // Quit the browser
-    await driver.quit();
-  }
+//     // Send the page title as the response
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/plain');
+//     res.end(pageTitle);
+//   } finally {
+//     // Quit the browser
+//     await driver.quit();
+//   }
 });
 
 server.listen(PORT, () => {
