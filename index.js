f0737adf0e678f0099edc7f94c0ba63179a5c42d
Mohammed EL YOUSFI <momarip.el.yousfi@gmail.com>
2023-05-05 14:20:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const chrome = require('selenium-webdriver/chrome');
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
+  let driver = await new Builder().forBrowser('chrome').setChromeOptions(new chrome.Options().headless()).build();
+  console.log(driver);
+  
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World!');
